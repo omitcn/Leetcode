256513ed4379d02176aa73372d9bdcd8eb767181
omit <registercn@outlook.com>
2024-08-15 09:51:36 +0800

    新增

--- Python/1. 两数之和/SolutionC.py
+++ Python/1. 两数之和/SolutionC.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums, target):
+        # 已遍历的数据
         num2index = {}
         for index, num in enumerate(nums):
-            another=target-num
+            another = target-num
             if another in num2index:
-                return [num2index[another],index]
-            num2index[num]=index
+                return [num2index[another], index]
+            num2index[num] = index
         return None
+
+
+if __name__ == '__main__':
+    nums = [2, 11, 3, 7, 21]
+    targets = 18
+    S = Solution()
+    a = S.twoSum(nums, targets)
+    print(a)
---
+++ Python/15. 三数之和/SolutionB.py
@@ -0,0 +1,8 @@
+class Solution:
+    def threeSum(self, nums):
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [-1, 0, 1, 2, -1, -4]
+    s.threeSum(nums=nums)
